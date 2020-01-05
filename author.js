a3c129ede5833b4406d5873a90a00f64d0cace00
Justin Corbin <jcorbin2290@gmail.com>
2020-01-05 14:51:39 -0700

    initial commit

--- author.js
+++ author.js
@@ -1,5 +1,17 @@
+var Book = require('./book.js');
+
+
 class Author {
+  constructor(newAuthor){
+    this.first = newAuthor.first;
+    this.last = newAuthor.last;
+    this.books = [];
+  }
+  write(title, year){
+    var newBook = new Book(this.first, this.last, title, year)
+    this.books.push(newBook);
+    return newBook;
 
+  }
 }
-
 module.exports = Author;
--- book.js
+++ book.js
@@ -1,5 +1,8 @@
 class Book {
-
+  constructor(first, last, title, publicationYear) {
+    this.author = `${first} ${last}`;
+    this.title = title;
+    this.publicationYear = publicationYear;
 }
-
+}
 module.exports = Book;
